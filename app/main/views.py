a69baa84c0ea73f055607c77c8675cdd3c31c627
wamakimaN <wamamoses@gmail.com>
2020-01-20 16:59:43 +0300

    add links and styling

--- app/main/views.py
+++ app/main/views.py
@@ -2,7 +2,7 @@ from flask import Flask, render_template
 from flask_bootstrap import Bootstrap
 from flask_wtf import FlaskForm
 from . import main
-from app.models import Post
+from app.models import Post,User
 from flask_login import login_required
 
 @main.route('/')
@@ -16,3 +16,14 @@ def index(id = None):
         title = 'Home'
 
     return render_template('index.html', posts=posts, title = title)
+
+@main.route('/user/<uname>', methods = ['GET','POST'])
+@login_required
+def profile(uname):
+
+    
+    user = User.query.filter_by(username = uname).first()
+    posts = Post.query.all()
+    
+
+    return render_template("profile/profile.html",posts = posts ,user = user)
